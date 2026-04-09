fb222efffc27886cf082d41d9ce59c4f05ccf4ee
ByteBreakeGhost <yasyfinance.9@gmail.com>
2026-04-09 20:42:58 +0700

    Create Belajar _ tumples.ts

---
+++ Study _ typescript/Belajar _ tumples.ts
@@ -0,0 +1,7 @@
+<! tumples ( array spesifik )
+// adalah array yang sudah di tentukan jumlah elementnya dan ururan type data.
+
+let user: [string, number, boolean];
+
+user = ["azhiim", 19, true]; // Benar
+// user = [true, "azhiim", 19]; // Error! Urutannya salah.
